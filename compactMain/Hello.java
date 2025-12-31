bdb03f1db0ca7c8304d3af9f5b68e157a8bd1398
margub9363 <69838623+margub9363@users.noreply.github.com>
2025-12-31 23:32:49 +0530

    compaict main

--- compactMain/Hello.java
+++ compactMain/Hello.java
@@ -1,3 +1,7 @@
+static String name = "Rahman";
+static String name2 = "Rahman2";
 void main() {
     System.out.println("Hello, Compact Java!");
+    System.out.println(name);
+    System.out.println(name2);
 }
